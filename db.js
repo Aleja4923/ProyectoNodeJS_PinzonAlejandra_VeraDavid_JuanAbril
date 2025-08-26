fb13b3caf9da385dc0bd8005d77d7065fb1b6f40
aleja4923 <alejapinzal2018@gmail.com>
2025-08-26 09:27:01 -0500

    feat: :sparkles: Frist Commit:

---
+++ db.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+
+const {MongoClient} = require('mongodb');
+
+const uri = process.env.URI;
+const dbName = process.env.DB_NAME;
+
+let client;
+let db;
+
+
+
+async function connect(){
+    if(db) return db; //Retorna la variable si tiene alguna conexión
+    client = new MongoClient(uri);//Importa MongoClient a client
+    await client.connect(); //Abre la conexión
+    db = client.db(dbName);//Selecciona y anida la BBDD
+    return db; // Retorna la conexión anidada a la BBDD
+}
+
+async function disconnect(){
+    if(client) await client.close();
+}
+
+connect();
+
+module.exports={connect,disconnect};
